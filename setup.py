75890ecd8ad4f8ae2618e25a41fd6cb2d3c8c473
Vasiliks <49211115+Vasiliks@users.noreply.github.com>
2020-04-11 17:12:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import setup_translate
 
 
 setup(name = 'enigma2-plugin-extensions-imagemanager',
-		version='2.6',
+		version='2.7',
 		author='Vasiliks',
 		author_email='vasiliks@narod.ru',
 		package_dir = {'Extensions.ImageManager': 'src'},
 		packages=['Extensions.ImageManager'],
-		package_data={'Extensions.ImageManager': ['icon/*.png'], ['bin/*']},
+		package_data={'Extensions.ImageManager': ['icon/*.png'], Extensions.ImageManager': ['bin/*'], Extensions.ImageManager': ['imagemanager.hlp']},
 		description = 'Create and manage your image enigma2',
 		cmdclass = setup_translate.cmdclass,
 	)
